6b9d84fe5c970bc90df698cc2b6b80af962b5924
yashasingh <yashasingh7@gmail.com>
2017-10-18 22:14:24 +0530

    pyc files added to gitignore

--- env/ApiKeyValues.py
+++
@@ -1,4 +0,0 @@
-access token = '885735452564570113-6uYKinA8J4443hJZy9bvxX0cd9aPIWD'
-access_secret = 'Vivycom682aU1NnsWNGCiEhjFuScesTywxFRxrRWlwxeC'
-consumer_key = 'phStB7lxmpx1WWCE78R9VPKn3'
-consumer_secret = 'lJMUIQzHuQ0n9ART8EiLaaT2aMMA3WFklDBZGAg3kuQ8t7Fpy8'
